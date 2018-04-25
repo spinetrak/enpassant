9c1c3e073d1e120cabeb0b5aa97b028c13f78731
spinetrak <spinetrak@spinetrak.net>
2018-04-25 21:59:30 +0200

    - trigger deployment from travis

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ssh $USER@$IP <<EOF
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $USER@$IP <<EOF
 cd $HOME/prod/enpassant/
 git pull https://github.com/spinetrak/enpassant.git
 mvn install
