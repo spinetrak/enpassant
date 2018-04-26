e8c5003a9ed4fa6a78c23b77bcfbe8d3f70e38ab
spinetrak <spinetrak@spinetrak.net>
2018-04-26 21:56:36 +0200

    - cleaner deploy

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,9 @@
 
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $MYUSER@$IP <<EOF
 cd /home/$MYUSER/prod/enpassant/
-git pull https://github.com/spinetrak/enpassant.git
+git fetch --all
+git reset --hard origin/master
+git pull origin master
 mvn install
 sudo systemctl restart enpassant
 EOF
